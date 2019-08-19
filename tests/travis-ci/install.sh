b233b450c598a9210e6c796d7891fa6441170ae3
cathysnider <catherine.snider@colorado.edu>
2019-08-19 15:08:19 -0600

    update drupal version from 7.66 to 7.67

--- tests/travis-ci/install.sh
+++ tests/travis-ci/install.sh
@@ -11,8 +11,8 @@ earlyexit
 
 # Build Codebase.
 cd $ROOT_DIR
-drush dl drupal-7.66
-mkdir drupal && mv drupal-7.66/* drupal/
+drush dl drupal-7.67
+mkdir drupal && mv drupal-7.67/* drupal/
 mkdir profiles && mv express_mono drupal/profiles/express
 
 # Harden Core.
