495e2eec61ed81b915c3c28abab3c695147739f4
kevincrafts <kevin.crafts@colorado.edu>
2020-07-07 10:09:33 -0600

    changing layout of multiselect collections !wip

--- modules/custom/express_collections_bundle/templates/collection-filter-links.tpl.php
+++ modules/custom/express_collections_bundle/templates/collection-filter-links.tpl.php
@@ -2,20 +2,16 @@
   <div class="collection-filter-links-wrapper collection-filter-links-multiple collection-filter-links-multi-select">
     <div class="collection-filter-links-inner">
     <h3 class="collection-filter-label" aria-controls="collection-<?php print $collection_id; ?>-filter-links">
-      <a href="#collection-<?php print $collection_id; ?>-filter-links" aria-expanded="false">
-        <span><?php print $label; ?></span>
-      </a>
+      <span><?php print $label; ?></span>
     </h3>
 
     <div class="collection-filter-links-form collection-filter-links" id="collection-<?php print $collection_id; ?>-filter-links">
-      <form>
         <?php foreach ($collection_filter_terms as $term): ?>
           <div class="filter">
             <input type="checkbox" id="<?php print _express_collections_bundle_clean_string($term->name); ?>" name="collection-<?php print $collection_id; ?>" value="<?php print _express_collections_bundle_clean_string($term->name); ?>" data-collection="collection-<?php print $collection_id; ?>">
             <label for="<?php print _express_collections_bundle_clean_string($term->name); ?>"><?php print $term->name; ?></label>
           </div>
         <?php endforeach; ?>
-      </form>
     </div>
 
 </div>
