23e8d3688a906ca6448a3976e95ef441c52c4362
brookestevens <brst4163@colorado.edu>
2020-09-10 18:23:05 -0600

    remove taxonomy references

--- modules/custom/cu_web_components_bean/bean--cu_web_components_bean.tpl.php
+++ modules/custom/cu_web_components_bean/bean--cu_web_components_bean.tpl.php
@@ -1,5 +1,4 @@
 <div class="vue-root" 
-    <?php print "data-wc-name='{$wc_name}' data-api-endpoint='{$api_endpoint}' data-id='{$resource_id}' "?> 
-    <?php $wc_data_option ? print "data-title='{$wc_data_option}'" : print "" ?>
+    <?php print "data-wc-name='{$wc_name}' data-api-endpoint='{$api_endpoint}' data-id='{$resource_id}' data-title='{$wc_data_option}' "?> 
 >
 </div>
--- modules/custom/cu_web_components_bean/plugins/bean/CUWebComponent.class.php
+++ modules/custom/cu_web_components_bean/plugins/bean/CUWebComponent.class.php
@@ -5,6 +5,7 @@
  * WC Bean.
  */
 class CUWebComponent extends BeanPlugin {
+    private $components, $data_options;
   
   /**
    * Declares default block settings. values must match the form ids or they wont save
@@ -13,11 +14,11 @@ class CUWebComponent extends BeanPlugin {
     $values = [
         'wc_name' => '',
         'wc_data_option' => '',
-        'resource_id' => ''
+        'resource_id' => '',
+        'api_endpoint' => ''
     ];
     return array_merge(parent::values(), $values);
   }
-  
 
   
   /**
@@ -28,12 +29,12 @@ class CUWebComponent extends BeanPlugin {
     $default = $this->getDefaultConfig($bean);
 
     $form['wc_name'] = [
-      '#type' => 'textfield',
+      '#type' => 'radios',
       '#title' => 'Web Component',
-      '#description' => 'Select the web component you want displayed',
-      '#autocomplete_path' => 'taxonomy/autocomplete/field_web_component',
+      '#args' => $this->data_options,
       '#default_value' => $bean->wc_name,
-      '#maxlength' => 1024,
+      '#description' => 'Select the web component you want displayed',
+      '#options' => $this->components,
       '#ajax' => array(
           'callback' => 'ajax_wc_data_options_callback',
           'wrapper' => 'checkboxes-div',
@@ -56,7 +57,13 @@ class CUWebComponent extends BeanPlugin {
        '#type' => 'select',
        '#options' => $default['options'],
        '#title' => t('Data Source Option'),
+       '#default_value' => $bean->wc_data_option,
        '#validated' => true, //prevent drupal from throwing an error from creating dynamic options
+    ];
+    
+    $form['data_options']['api_endpoint'] = [
+        '#type' => 'hidden',
+        '#default_value' =>$bean->api_endpoint
     ];
     
     $form['resource_id'] = [
@@ -64,8 +71,7 @@ class CUWebComponent extends BeanPlugin {
         '#title' => 'Resource ID',
         '#default_value' => $bean->resource_id,
         '#description' => 'The ID of the data that you want to get'
-    ];
-    
+    ];    
     return $form;
   }
   
@@ -74,51 +80,54 @@ class CUWebComponent extends BeanPlugin {
   // set default options if the user is editing a bean
   // @return {array} returns the proper default description and options array
   public function getDefaultConfig($bean){
-        $default = [
-            'options' => ['default' => '-- WC Data Options --'],
-            'desc' => 'Some api endpoints can return data on more than one subject. Please select the Web component you want to show first.'
-        ];
-        if(!empty($bean->wc_name)){
-            $vocabulary = taxonomy_vocabulary_machine_name_load('web_component_data_options');
-            $terms = entity_load('taxonomy_term', FALSE, array('vid' => $vocabulary->vid));
-            $arr =[];
-            foreach($terms as $i){
-                $wc = taxonomy_term_load(($i->field_web_component)['und'][0]['tid']);
-                if($wc->name === $default){
-                    $arr[$i->tid] = $i->name;
-                }
-            }
-            if(count($arr) > 0){
-                $default['options'] = $arr;
-                $default['desc'] = 'Please select what data you want to show from this data source';
-            }
-            else{
-                $default['desc'] = 'There are no data options for this web component';
+    $res = drupal_http_request('https://spreadsheets.google.com/feeds/list/1wj5ZwMhf3D-KN_LGjweCEhvCEW0wTp3u-Wz4SzBAtRo/1/public/values?alt=json');
+    if($res->code !== '200'){
+        drupal_set_message(t('Could not retrieve available Web Components. Please contact your site admin.'), 'error');
+        $this->components = [];
+    }
+    else{
+        $res = json_decode($res->data, true);
+        foreach($res['feed']['entry'] as $i){
+            $this->components[$i['gsx$webcomponentname']['$t']] = $i['gsx$webcomponentlabel']['$t'];
+            $this->data_options[$i['gsx$webcomponentname']['$t']] = ['options' => $i['gsx$dataoptions']['$t'], 'api_endpoint' => $i['gsx$apiendpoint']['$t'] ];
+        };
+    }
+    $default = [
+        'options' => ['default' => '-- WC Data Options --'],
+        'desc' => 'Some api endpoints can return data on more than one subject. Please select the Web component you want to show first.'
+    ];
+    if(!empty($bean->wc_name)){
+        $options = []; 
+        $arr = [];
+        foreach($this->data_options as $key => $i){
+            if($key == $bean->wc_name && $i['options'] !== 'none'){
+                $arr = explode(', ', $i['options']);
+                break;
             }
         }
-        return $default;
-  }
+        if(count($arr) > 0){
+            foreach($arr as $key => $i){
+                $options[$i] = $i;
+            }
+            $default['options'] = $options;
+            $default['desc'] = 'Please select what data you want to show from this data source';
+        }
+        else{
+            $default['desc'] = 'There are no data options for this web component';
+        }
+    }
+    return $default;
+}
 
   /**
    * Displays the bean.
    */
   public function view($bean, $content, $view_mode = 'default', $langcode = NULL) {
-    $wc_name = $url = '';
-    $vid = taxonomy_vocabulary_machine_name_load('web_components')->vid;
-    $terms = entity_load('taxonomy_term', FALSE, array('vid' => $vid));
-    foreach($terms as $i){
-        if($i->name === $bean->wc_name){
-            $wc_name = $i->field_wc_name['und'][0]['value'];
-            $url = $i->field_api_endpoint['und'][0]['value'];
-            break;
-        }
-    }
-    
     $content['#theme'] = 'bean__cu_web_components_bean';
-    $content['#wc_name'] = $wc_name;
+    $content['#wc_name'] = $bean->wc_name;
     $content['#resource_id'] = $bean->resource_id;
-    $content['#api_endpoint'] = str_replace('[id]', $bean->resource_id, $url);
-    $content['#wc_data_option'] = !$bean->wc_data_option ? null : taxonomy_term_load($bean->wc_data_option)->name;
+    $content['#api_endpoint'] = str_replace('[id]', $bean->resource_id, $bean->api_endpoint);
+    $content['#wc_data_option'] = $bean->wc_data_option;
 
     return $content;
   }
