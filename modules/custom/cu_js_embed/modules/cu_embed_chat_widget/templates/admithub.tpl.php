6f2e01308b1b9eab6e3377d3bf2e9d53085923df
kevincrafts <kevin.crafts@colorado.edu>
2020-09-30 14:08:48 -0600

    adding chat widgets !wip

---
+++ modules/custom/cu_js_embed/modules/cu_embed_chat_widget/templates/admithub.tpl.php
@@ -0,0 +1,3 @@
+<script>window.admitHubBot = {botToken: "<?php print $account_id; ?>" };</script>
+<script src="https://webbot.admithub.com/static/js/webchat.js"></script>
+<link rel="stylesheet" type="text/css" href="https://webbot.admithub.com/static/css/webchat.css">
---
+++ modules/custom/cu_js_embed/modules/cu_embed_chat_widget/templates/livechat.tpl.php
@@ -0,0 +1,6 @@
+var __lc = {}; __lc.license = <?php print $account_id; ?>;
+(function() {
+  var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+  lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+})();
