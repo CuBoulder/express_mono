4047af399ade346d5c951ca61135cc06fb7a0601
AlanBCole <alantuenie@gmail.com>
2020-10-30 12:09:15 -0600

    added github workflow files

---
+++ .github/workflows/action-apply-contrib-patches/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ git diff -r --quiet dev_alans..  modules/contrib ]
+then
+    echo "No patches need to be applied."
+else
+    echo "Applying patches..."
+    git apply patches/bean-integration-2127731-10.patch
+fi
