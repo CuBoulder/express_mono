d87f58214003acf222c3a6d5ab6ba4527c948569
AlanBCole <alantuenie@gmail.com>
2020-11-02 10:44:55 -0700

    change to entrypoint.sh if condition. Using

--- .github/workflows/action-apply-contrib-patches/entrypoint.sh
+++ .github/workflows/action-apply-contrib-patches/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $(git diff -r --quiet dev_alans..  modules/contrib) ]
+if [ "git diff -r --quiet dev_alans..$GITHUB_REF  modules/contrib" ]
 then
     echo "No patches need to be applied."
 else
