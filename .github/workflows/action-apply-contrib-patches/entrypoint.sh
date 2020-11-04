2fe49df9cb79f6fc8487375eb057556e2abde5b1
AlanBCole <alantuenie@gmail.com>
2020-11-04 11:01:39 -0700

    changing if condition to be git add .

--- .github/workflows/action-apply-contrib-patches/entrypoint.sh
+++ .github/workflows/action-apply-contrib-patches/entrypoint.sh
@@ -5,21 +5,21 @@ git config --global user.name "osrwebdeploy"
 
 echo "ACTIVE GIT BRANCH"
 git branch
-
-echo "GITHUB_REF env variable -> $GITHUB_REF"
 echo "\n\n"
 
-if  "git diff -r --quiet dev_alans..$GITHUB_REF  modules/contrib"
+# git diff -r --quiet dev_alans..  modules/contrib || git apply patches/*
+if git add .
 then
-    echo "No patches need to be applied."
+    echo "Then block"
 else
-    echo "Applying patches..."
-    git apply patches/*
-    git add .
-    git commit -m "applying patches"
-    git push origin $GITHUB_REF
+    echo "Else block"
+    # echo "Applying patches..."
+    # git apply patches/*
+    # git add .
+    # git commit -m "applying patches"
+    # git push origin $GITHUB_REF
 
-    echo "Hopefully, it looks good!"
+    # echo "Hopefully, it looks good!"
 fi
 
 echo "See ya!"
