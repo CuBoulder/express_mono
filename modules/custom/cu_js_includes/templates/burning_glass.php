3c78c1b8a41207278e29bbe871dae963300ac2ed
AlanBCole <alantuenie@gmail.com>
2020-11-16 10:48:44 -0700

    new: adding Burning Glass Javascript Include type. Resolves #635
    Added burning_glass to cu_js_includes_enabled in .install
    Added burning_glass to  array in .module file at cu_js_includes_block_options()
    Added cu_js_includes_burning_glass_form() in .forms.inc file
    Added view burning_glass js includes to cu_js_includes_secure_permissions() in .permissions.inc file
    Added  to cu_js_includes_get_include_types() in .types.inc file
    Added Add Burning GLass Include to [types] array in cu_js_includes_express_node_list_groups() in .types.inc file
    Added burning glass php template in /templates/burning_glass.php

---
+++ modules/custom/cu_js_includes/templates/burning_glass.php
@@ -0,0 +1,12 @@
+<div id="ci"></div>
+<script src="https://careerinsight.burning-glass.com/enroll/scripts/cienroll.js"></script>
+<script>
+    document.body.onload = function() {
+        var props = {
+            accessCode: $access_code,
+            programCode: $program_code,
+            elementId: 'ci'
+        }
+        openCI(props);
+    }
+</script>
