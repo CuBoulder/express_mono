be2aebba54e7e9e6df306309c7ffab0802edae78
AlanBCole <alantuenie@gmail.com>
2020-11-20 14:33:39 -0700

    fix: Using a different and simpler approach. Resolves #742.

--- modules/features/people_content_type/templates/node--person.tpl.php
+++ modules/features/people_content_type/templates/node--person.tpl.php
@@ -1,6 +1,6 @@
 <?php hide($content['links']); ?>
-<div class="row person-container">
-  <div class="person-photo col-sm-4 col-xs-6"><?php print render($content['field_person_photo']); ?></div>
+<div class="person-container">
+  <div class="person-photo"><?php print render($content['field_person_photo']); ?></div>
 
   <?php
     // Add check for $person_title due to warning notice.
@@ -16,42 +16,40 @@
     $title = join(' &bull; ' , $person_title);
     //print $title;
   ?>
-  <div class="person-info col-sm-8">
-    <div class="person-job-titles"><?php print $content['job_titles']; ?></div>
-    <div class="person-departments"><?php print $content['departments']; ?></div>
-    <?php if(!empty($content['field_person_email']) || !empty($content['field_person_phone'])): ?>
-    <div class="people-contact people-section">
-      <?php if(!empty($content['field_person_email'])): ?>
-        <div class="person-email person-contact-info-item"><i class="fa fa-envelope fa-fw"></i> <?php print render($content['field_person_email']); ?></div>
-      <?php endif; ?>
-      <?php if(!empty($content['field_person_phone'])): ?>
-        <div class="person-phone person-contact-info-item"><i class="fa fa-phone fa-fw"></i> <?php print render($content['field_person_phone']); ?></div>
-      <?php endif; ?>
-    </div>
+  <div class="person-job-titles"><?php print $content['job_titles']; ?></div>
+  <div class="person-departments"><?php print $content['departments']; ?></div>
+  <?php if(!empty($content['field_person_email']) || !empty($content['field_person_phone'])): ?>
+  <div class="people-contact people-section">
+    <?php if(!empty($content['field_person_email'])): ?>
+      <div class="person-email person-contact-info-item"><i class="fa fa-envelope fa-fw"></i> <?php print render($content['field_person_email']); ?></div>
     <?php endif; ?>
-    
-    <?php if(!empty($content['field_person_website'])): ?>
-    <div class="people-links people-section">
-      <?php print render($content['field_person_website']); ?>
-    </div>
-    <?php endif; ?>
-    
-    <?php if(!empty($content['field_person_address']) || !empty($content['field_person_office_hours'])): ?>
-    <div class="people-office people-section">
-      <?php if(!empty($content['field_person_address'])): ?>
-        <?php print render($content['field_person_address']); ?>
-      <?php endif; ?>
-      <?php if(!empty($content['field_person_office_hours'])): ?>
-        <?php print render($content['field_person_office_hours']); ?>
-      <?php endif; ?>
-    </div>
-    <?php endif; ?>
-    
-    
-    <?php if(!empty($content['body'])): ?>
-      <div class="people-bio">
-        <?php print render($content['body']); ?>
-      </div>
+    <?php if(!empty($content['field_person_phone'])): ?>
+      <div class="person-phone person-contact-info-item"><i class="fa fa-phone fa-fw"></i> <?php print render($content['field_person_phone']); ?></div>
     <?php endif; ?>
   </div>
-</div> <!-- row end -->
+  <?php endif; ?>
+
+  <?php if(!empty($content['field_person_website'])): ?>
+  <div class="people-links people-section">
+    <?php print render($content['field_person_website']); ?>
+  </div>
+  <?php endif; ?>
+
+  <?php if(!empty($content['field_person_address']) || !empty($content['field_person_office_hours'])): ?>
+  <div class="people-office people-section">
+    <?php if(!empty($content['field_person_address'])): ?>
+      <?php print render($content['field_person_address']); ?>
+    <?php endif; ?>
+    <?php if(!empty($content['field_person_office_hours'])): ?>
+      <?php print render($content['field_person_office_hours']); ?>
+    <?php endif; ?>
+  </div>
+  <?php endif; ?>
+
+
+  <?php if(!empty($content['body'])): ?>
+    <div class="people-bio">
+      <?php print render($content['body']); ?>
+    </div>
+  <?php endif; ?>
+</div>
